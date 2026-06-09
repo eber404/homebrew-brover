30e22182370f493184864e44685a68089bca10ab
eber404 <bodacamargo@gmail.com>
2026-06-09 16:21:08 -0300

    chore: add brover cask for v0.1.4

--- Casks/brover.rb
+++ Casks/brover.rb
@@ -5,10 +5,10 @@ cask "brover" do
   sha256 arm: "482f6f6939480d191c7bf0e2dd6d82abc13be488cb5965e5e2f394df11a3933d",
          intel: "09bfeddafbad090e1ae0b5d3b3cad92fd1528fd3c5cac3fa0eae8248ff664852"
 
-  url "https://github.com/eber404/brover-releases/releases/download/v#{version}/Brover-#{version}-#{arch}.dmg"
+  url "https://github.com/eber404/homebrew-brover/releases/download/v#{version}/Brover-#{version}-#{arch}.dmg"
   name "Brover"
   desc "Local environment secret manager for macOS"
-  homepage "https://github.com/eber404/brover-releases"
+  homepage "https://github.com/eber404/homebrew-brover"
 
   app "Brover.app"
 
