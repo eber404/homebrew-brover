dd4fbbd9b847cd28151bac8b7816fb27444f6530
eber404 <bodacamargo@gmail.com>
2026-06-09 08:08:34 -0300

    fix: align brover cask checksums

--- Casks/brover.rb
+++ Casks/brover.rb
@@ -2,8 +2,8 @@ cask "brover" do
   arch arm: "arm64", intel: "x64"
 
   version "0.1.2"
-  sha256 arm: "4c4696b39b49969e74e3913bdb0eacef16da55537a2f5da10fe153ea16d4bd43",
-         intel: "c0300dfbbb4d1de47d527becfc30175c981ca351adfac30d59aff5160a652cb3"
+  sha256 arm: "88ac6dd4e7407a22099836d37bd6b9fe0eec2dd06ac6f8d57285d9a1545a2c81",
+         intel: "78edd1233d4766d2daeac3261deeefd83c7b2a5e4460633fb4c0c1d8ff71fe4b"
 
   url "https://github.com/eber404/brover-releases/releases/download/v#{version}/Brover-#{version}-#{arch}.dmg"
   name "Brover"
