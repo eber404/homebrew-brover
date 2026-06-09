7c34ad7d508ca34d9f973a66360e25a85832898f
eber404 <bodacamargo@gmail.com>
2026-06-09 00:28:29 -0300

    fix: publish cask from public release repo

--- Casks/brover.rb
+++ Casks/brover.rb
@@ -5,14 +5,12 @@ cask "brover" do
   sha256 arm: "33068224c110e4e98b3fe3d23a2d50b941d727bd45e3039fe22db2bfcfa34236",
          intel: "bc56fee699d85284f8db12546b0be72d7d6845db803f41c2947bb878488e67ef"
 
-  url "https://github.com/eber404/brover/releases/download/v#{version}/Brover-#{version}-#{arch}.dmg"
+  url "https://github.com/eber404/brover-releases/releases/download/v#{version}/Brover-#{version}-#{arch}.dmg"
   name "Brover"
   desc "Local environment secret manager for macOS"
-  homepage "https://github.com/eber404/brover"
+  homepage "https://github.com/eber404/brover-releases"
 
   app "Brover.app"
 
-  zap trash: [
-    "~/Library/Application Support/brover",
-  ]
+  zap trash: "~/Library/Application Support/brover"
 end
